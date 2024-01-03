6cbda4c25b1cb259247a6ea961d3411295a6acff
Fermin Blanco <luillyfe89@gmail.com>
2024-01-03 12:45:43 -0500

    fix: remove duplicate class.

--- src/app/answer.tsx
+++ src/app/answer.tsx
@@ -14,7 +14,7 @@ export default function Answer({ text, id }: { text: string; id: string }) {
         value={id}
         className="hidden w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
       />
-      <label htmlFor={id} className={`mb-3 text-2xl text-sm`}>
+      <label htmlFor={id} className={`mb-3 text-sm`}>
         {text}
       </label>
     </div>
