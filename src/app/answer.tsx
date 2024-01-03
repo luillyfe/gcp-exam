5f1e7ae84dcea99b90e41e9f8b36c53af8b5927f
Fermin Blanco <luillyfe89@gmail.com>
2024-01-03 12:44:58 -0500

    feature: adding answer component.

---
+++ src/app/answer.tsx
@@ -0,0 +1,22 @@
+export default function Answer({ text, id }: { text: string; id: string }) {
+  return (
+    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+      {/* TODO: review semantic html tags */}
+      <h2 className={`mb-3 text-2xl font-semibold uppercase`}>
+        {id}{" "}
+        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          -&gt;
+        </span>
+      </h2>
+      <input
+        id={id}
+        type="radio"
+        value={id}
+        className="hidden w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      />
+      <label htmlFor={id} className={`mb-3 text-2xl text-sm`}>
+        {text}
+      </label>
+    </div>
+  );
+}
