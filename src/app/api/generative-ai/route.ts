4e2bf59508a9bbb296ab5de4ca09b621bfc30b45
Fermin Blanco <luillyfe89@gmail.com>
2024-01-16 10:54:17 -0500

    feature: adding serverless function to talk to generative ai service: Gemini.

--- src/app/api/generative-ai/route.ts
+++ src/app/api/generative-ai/route.ts
@@ -1,5 +1,14 @@
 import { NextResponse } from "next/server";
 
+import { AIModelResponse } from "@/app/lib/definitions";
+
+import {
+  getTextBetweenBackticks,
+  validateResponse,
+} from "@/app/lib/utils/textUtils";
+
+import { insertIntoDatabase } from "@/app/lib/database";
+
 import {
   GoogleGenerativeAI,
   HarmCategory,
@@ -8,12 +17,65 @@ import {
 
 const MODEL_NAME = process.env.MODEL_NAME || "";
 const API_KEY = process.env.API_KEY || "";
-const PROMPT = process.env.PROMPT || "";
+// Chat history
+const PART_1_USER = process.env.PART_1_USER || "";
+const PART_1_MODEL = process.env.PART_1_MODEL || "";
+const PART_2_USER = process.env.PART_2_USER || "";
+const PART_2_MODEL = process.env.PART_2_MODEL || "";
+const PART_3_USER = process.env.PART_3_USER || "";
+const PART_3_MODEL = process.env.PART_3_MODEL || "";
+const PART_4_USER = process.env.PART_4_USER || "";
+const PART_4_MODEL = process.env.PART_4_MODEL || "";
+const PART_5_USER = process.env.PART_5_USER || "";
+const PART_5_MODEL = process.env.PART_5_MODEL || "";
 
+// GET /api/generative-ai
 export async function GET() {
   const response = await run();
+  let json;
 
-  return NextResponse.json(JSON.parse(response), {
+  // parse JSON from text response
+  try {
+    const JSONString = getTextBetweenBackticks(response) || "{}";
+    json = JSON.parse(JSONString);
+  } catch {
+    return NextResponse.json(
+      { message: "Error parsing response" },
+      {
+        status: 501,
+      }
+    );
+  }
+
+  // validate that the json has the expected properties
+  let validated: Array<AIModelResponse>;
+  try {
+    // @ts-expect-error: type coercion is valid
+    validated = validateResponse(json);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error validating response" },
+      {
+        status: 501,
+      }
+    );
+  }
+
+  try {
+    await insertIntoDatabase(validated);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error inserting into database" },
+      {
+        status: 501,
+      }
+    );
+  }
+
+  // success: return response
+  return NextResponse.json(json, {
     status: 200,
   });
 }
@@ -26,7 +88,7 @@ async function run() {
     temperature: 0.9,
     topK: 1,
     topP: 1,
-    maxOutputTokens: 2048,
+    maxOutputTokens: 4096,
   };
 
   const safetySettings = [
@@ -48,13 +110,96 @@ async function run() {
     },
   ];
 
-  const parts = [{ text: PROMPT }];
-
-  const result = await model.generateContent({
-    contents: [{ role: "user", parts }],
+  const chat = model.startChat({
     generationConfig,
     safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [
+          {
+            text: PART_1_USER,
+          },
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {
+            text: PART_1_MODEL,
+          },
+        ],
+      },
+      {
+        role: "user",
+        parts: [
+          {
+            text: PART_2_USER,
+          },
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {
+            text: PART_2_MODEL,
+          },
+        ],
+      },
+      {
+        role: "user",
+        parts: [
+          {
+            text: PART_3_USER,
+          },
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {
+            text: PART_3_MODEL,
+          },
+        ],
+      },
+      {
+        role: "user",
+        parts: [
+          {
+            text: PART_4_USER,
+          },
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {
+            text: PART_4_MODEL,
+          },
+        ],
+      },
+      {
+        role: "user",
+        parts: [
+          {
+            text: PART_5_USER,
+          },
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {
+            text: PART_5_MODEL,
+          },
+        ],
+      },
+    ],
   });
+
+  const result = await chat.sendMessage(
+    "Dear Gemini, please generate 5 more questions."
+  );
 
   const response = result.response;
   return response.text();
